4d45c61507104c69f16105de864a842fddebecef
Christian Zagrodnick <cz@flyingcircus.io>
2008-09-19 06:43:21 +0000

    added deployment support

--- gocept/nginx/recipe.py
+++ gocept/nginx/recipe.py
@@ -22,41 +22,62 @@ class Recipe(object):
     """
 
     def __init__(self, buildout, name, options):
-        self.buildout = buildout
         self.name = name
         self.options = options
 
-        location = options["location"] = os.path.join(
-            buildout["buildout"]["parts-directory"], name)
-
-        if not os.path.exists(location):
-            os.mkdir(location)
+        deployment = self.deployment = options.get('deployment')
+        if deployment:
+            self.deployment = buildout[deployment].get('name', deployment)
+            options['etc-directory'] = buildout[deployment]['etc-directory']
+            options['rc-directory'] = buildout[deployment]['rc-directory']
+            options['run-directory'] = buildout[deployment]['run-directory']
+            options['user'] = buildout[deployment]['user']
+        else:
+            options['etc-directory'] = options["run-directory"] = os.path.join(
+                buildout["buildout"]["parts-directory"], name)
+            options['rc-directory'] = buildout['buildout']['bin-directory']
 
         options.setdefault('nginx', 'nginx')
-        options.setdefault('config_file', os.path.join(options['location'],
-                                                       'nginx.conf'))
-        options.setdefault('pid_file', os.path.join(options['location'],
-                                                    'nginx.pid'))
-        options.setdefault('ngnix_location', os.path.join(
+        options.setdefault('nginx_location', os.path.join(
             buildout["buildout"]["parts-directory"], options['nginx']))
 
     def install(self):
-        location = self.options["location"]
+        if self.deployment:
+            prefix = self.deployment + '-'
+        else:
+            prefix = ""
+            if not os.path.exists(self.options['run-directory']):
+                os.mkdir(self.options['run-directory'])
+                self.options.created(self.options['run-directory'])
+
+        config_path = os.path.join(
+            self.options['etc-directory'],
+            prefix+self.name+'.conf')
+        ctl_path = os.path.join(self.options["rc-directory"],
+                                prefix+self.name)
+        pid_path = os.path.join(
+            self.options['run-directory'], prefix+self.name+'.pid')
+
+
         # Write the configuration file
-        conf_path = os.path.join(self.options['config_file'])
-        config_file = file(conf_path, 'w')
-        config_file.write('pid %s;\n' % self.options['pid_file'])
+        config_file = file(config_path, 'w')
+        config_file.write('pid %s;\n' % pid_path)
+        if self.deployment:
+            config_file.write('user %s;\n' % self.options['user'])
         config_file.write(self.options['configuration'])
+        config_file.close()
+        self.options.created(config_path)
 
         # files
-        ctl_path = os.path.join(self.buildout["buildout"]["bin-directory"],
-                                self.name)
-        open(ctl_path, "w").write(read_resource("nginxctl.in") % self.options)
+        open(ctl_path, "w").write(read_resource("nginxctl.in") % dict(
+            nginx_location=self.options['nginx_location'],
+            pid_file=pid_path,
+            config_file=config_path))
 
         os.chmod(ctl_path, (os.stat(ctl_path).st_mode |
                             stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH))
-
-        return [ctl_path]
+        self.options.created(ctl_path)
+        return self.options.created()
 
     def update(self):
         pass
